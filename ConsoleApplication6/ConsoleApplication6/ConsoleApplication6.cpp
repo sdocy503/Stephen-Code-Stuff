77f2de050d3401eb2bc77595358c6da380df531c
sdocy503 <docystephen@gmail.com>
2018-01-18 14:05:16 -0800

    Chapter 5 optimizations

--- ConsoleApplication6/ConsoleApplication6/ConsoleApplication6.cpp
+++ ConsoleApplication6/ConsoleApplication6/ConsoleApplication6.cpp
@@ -16,9 +16,7 @@ using namespace std;
 int main() {
 	ifstream dataFile;
 	ofstream output;
-	vector<string> in;
 	string inString;
-	vector<string> words;
 	string firstName;
 	string lastName;
 	double percent;
@@ -29,36 +27,28 @@ int main() {
 	output.open("Ch3_Ex6Output.dat");
 	while (dataFile.eof() != true) {
 		getline(dataFile, inString);
-		in.push_back(inString);
-
-		while (in.size() > 0) {
-			//cout << in.back() << endl;
-			//in.resize(in.size() - 1);
-
-			istringstream iss(in.back());
-			vector<string> results((istream_iterator<string>(iss)),
-				istream_iterator<string>());
-			//cout << results.back() << endl;
-			cout << results[2] << endl;
-			firstName = results[1];
-			lastName = results[0];
-			//string payString = results[2];
-			//string percentString = results[3];
-			pay = stod(results[2]);
-			percent = stod(results[3]);
+		istringstream iss(inString);
+		vector<string> results((istream_iterator<string>(iss)),
+			istream_iterator<string>());
+		//cout << results.back() << endl;
+		cout << results[2] << endl;
+		firstName = results[1];
+		lastName = results[0];
+		//string payString = results[2];
+		//string percentString = results[3];
+		pay = stod(results[2]);
+		percent = stod(results[3]);
 			
-			//pay = 80000.75f;
-			cout << fixed;
-			cout << setprecision(2) << firstName << " " << lastName << " " << pay << " " << percent << endl;
-			if (percent >= 6.0) {
-				pay = pay + 1000.0;
-			}
-			output << fixed;
-			output << setprecision(2) << firstName << " " << lastName << " " << pay << endl;
-			results.resize(0);
-			
-			in.resize(in.size() - 1);
+		//pay = 80000.75f;
+		cout << fixed;
+		cout << setprecision(2) << firstName << " " << lastName << " " << pay << " " << percent << endl;
+		if (percent >= 6.0) {
+			pay = pay + 1000.0;
 		}
+		output << fixed;
+		output << setprecision(2) << firstName << " " << lastName << " " << pay << endl;
+		results.resize(0);
+		
 	}
 
 	return 0;
